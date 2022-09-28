02b151587bc8477a7d25829ab5b53359fd7012bb
nubymariely@gmail.com <nubymariely@gmail.com>
2022-09-28 15:36:13 -0400

    simple basic project

---
+++ src/class03B4/class03.java
@@ -0,0 +1,12 @@
+package class03B4;
+
+public class class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey this is a git class");
+
+	}
+
+}
